e6ed4afe8e28a814b11cba72faeec73e0e6b19cd
ourTeam <68965830+ourTeam99@users.noreply.github.com>
2020-08-21 06:32:36 +0300

    send video request

--- server/models/video-requests.model.js
+++ server/models/video-requests.model.js
@@ -1,4 +1,4 @@
-var mongoose = require('./mongo.config');
+var mongoose = require("./mongo.config");
 
 if (!Object.keys(mongoose).length) return;
 
@@ -13,15 +13,15 @@ var VideoRequestsSchema = mongoose.Schema(
       ups: { type: Number, default: 0 },
       downs: { type: Number, default: 0 },
     },
-    target_level: { type: String, default: 'beginner' },
-    status: { type: String, default: 'new' },
+    target_level: { type: String, default: "beginner" },
+    status: { type: String, default: "new" },
     video_ref: {
-      link: { type: String, default: '' },
-      date: { type: String, default: '' },
+      link: { type: String, default: "" },
+      date: { type: String, default: "" },
     },
   },
-  { timestamps: { createdAt: 'submit_date', updatedAt: 'update_date' } }
+  { timestamps: { createdAt: "submit_date", updatedAt: "update_date" } }
 );
 
-var videoRequestsModel = mongoose.model('VideoRequests', VideoRequestsSchema);
+var videoRequestsModel = mongoose.model("VideoRequests", VideoRequestsSchema);
 module.exports = videoRequestsModel;
